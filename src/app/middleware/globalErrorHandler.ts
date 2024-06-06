9c9bdb9bdb9c86a4757fd9ddccd462c8cc945507
Bulbul-Programing <bulbul.programing@gmail.com>
2024-06-06 16:00:38 +0600

    some change

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,13 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSource } from "../interface/error";
 
 const globalErrorHandler = (err : any, req :Request, res : Response, next: NextFunction)=>{
   
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Something is wrong'
-  
+
+
+    let errorSource : TErrorSource = {
+        path : '',
+        message : 'something is wrong'
+    }
+
+
     return res.status(statusCode).json({
       success : false,
       message : message,
+      errorSource : errorSource,
       error : err
     })
   }
