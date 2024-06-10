55b91c0537fd2bc6cba2ab4a9daa9581bdbd4023
Bulbul-Programing <bulbul.programing@gmail.com>
2024-06-10 21:52:58 +0600

    create course validtaion schema create

---
+++ src/app/modules/course/course.validation.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+
+const preRequisiteCourseSchema = z.object({
+    title : z.string(),
+    isDeleted : z.string().optional()
+})
+
+const createCourseValidationSchema = z.object({
+  body: z.object({
+    title: z.string(),
+    prefix: z.string(),
+    code: z.number(),
+    credit: z.number(),
+    isDeleted: z.array(preRequisiteCourseSchema)
+  }),
+})
+
+export const createCourseValidation = {
+    createCourseValidationSchema
+}
