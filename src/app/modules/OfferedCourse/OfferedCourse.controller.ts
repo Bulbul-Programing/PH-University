abf63a834896f32295c7fceb0eac53f5b6d04ab6
Bulbul Ahammed Riad <bulbulprograming@gmail.com>
2024-06-17 11:27:09 +0600

    offered course created succefully

--- src/app/modules/OfferedCourse/OfferedCourse.controller.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.controller.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
+import { offerCourseService } from "./OfferedCourse.service";
 
 
 const createOfferedCourse = catchAsync(async (req: Request, res: Response) => {
+    const result = await offerCourseService.createOfferedCourseIntoDB(req.body)
 
+    res.status(200).json({
+        success: true,
+        massage: 'Offered course created successfully',
+        data: result
+    })
 })
 
 
