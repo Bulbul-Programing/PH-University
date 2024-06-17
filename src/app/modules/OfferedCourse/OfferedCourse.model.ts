82397fd98dba9aa529a49fa10ab3b53c126a6844
Bulbul Ahammed Riad <bulbulprograming@gmail.com>
2024-06-17 15:58:12 +0600

    offered course multiple validation

--- src/app/modules/OfferedCourse/OfferedCourse.model.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.model.ts
@@ -5,14 +5,13 @@ import { Days } from "./OfferedCourse.const";
 
 const offerCourseSchema = new Schema<TOfferedCourse>({
     semesterRegistration: { type: Schema.Types.ObjectId, required: true, ref: 'SemesterRegistration' },
-    academicSemester: { type: Schema.Types.ObjectId, required: true, ref: 'AcademicSemester' },
     academicFaculty: { type: Schema.Types.ObjectId, required: true, ref: 'academicFaculty' },
     academicDepartment: { type: Schema.Types.ObjectId, required: true, ref: 'academicFaculty' },
     course: { type: Schema.Types.ObjectId, required: true, ref: 'courseFaculty' },
     faculty: { type: Schema.Types.ObjectId, required: true, ref: 'faculty' },
     maxCapacity: { type: Number, required: true, default : 10 },
     section: { type: Number, required: true },
-    days: { type: String, required: true , enum : Days},
+    days: [{type: String, required: true , enum : Days}],
     startTime: { type: String, required: true },
     endTime: { type: String, required: true }
 },{
--- src/app/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -1,9 +1,74 @@
+import AppError from "../../error/AppError";
+import { FacultyModel } from "../Faculty/faculty.model";
+import { AcademicDepartmentModel } from "../academicDepartment/academicDepartment.model";
+import { academicFacultyModel } from "../academicFaculty/academicFaculty.model";
+import { courseModel } from "../course/course.model";
+import { SemesterRegistrationModel } from "../semesterRegistration/semesterRegistration.model";
 import { TOfferedCourse } from "./OfferedCourse.interface";
 import { OfferedCourseModel } from "./OfferedCourse.model";
 
 
 const createOfferedCourseIntoDB = async (payload : TOfferedCourse) => {
-    const result = await OfferedCourseModel.create(payload)
+    const {semesterRegistration, academicFaculty, academicDepartment, course, faculty, section} = payload
+    
+    // checking semester registration is exist
+    const isSemesterRegistrationExist = await SemesterRegistrationModel.findById(semesterRegistration)
+    if(!isSemesterRegistrationExist){
+        throw new AppError(404, 'Semester Registration not Found!')
+    }
+    
+    // checking academic Faculty is exist
+    const isAcademicFacultyExist = await academicFacultyModel.findById(academicFaculty)
+    if(!isAcademicFacultyExist){
+        throw new AppError(404, 'academic Faculty not Found!')
+    }
+    
+    // checking academic department is exist
+    const isAcademicDepartmentExist = await AcademicDepartmentModel.findById(academicDepartment)
+    if(!isAcademicDepartmentExist){
+        throw new AppError(404, 'academic department not Found!')
+    }
+    
+    // checking academic course is exist
+    const isCourseExist = await courseModel.findById(course)
+    if(!isCourseExist){
+        throw new AppError(404, 'course not Found!')
+    }
+    
+    // checking academic faculty is exist
+    const isFacultyExist = await FacultyModel.findById(faculty)
+    if(!isFacultyExist){
+        throw new AppError(404, 'faculty not Found!')
+    }
+
+    const academicSemester = isSemesterRegistrationExist.academicSemester
+
+    // check if the department is belong to the faculty
+    const isDepartmentBelongToFaculty = await AcademicDepartmentModel.findOne({
+        _id: academicDepartment,
+        academicFaculty,
+      });
+    
+      if (!isDepartmentBelongToFaculty) {
+        throw new AppError(
+          500,
+          `This ${isAcademicDepartmentExist.name} is not  belong to this ${isAcademicFacultyExist.name}`,
+        );
+      }
+
+      // checking same semester registration same course and same section is already exist.
+
+      const isSameOfferedCourseExistWithSameRegisteredWithSameSection = await OfferedCourseModel.findOne({
+        semesterRegistration,
+        course,
+        section
+      })
+
+      if(isSameOfferedCourseExistWithSameRegisteredWithSameSection){
+        throw new AppError(500, 'offered course with same section is already exist!')
+      }
+
+    const result = await OfferedCourseModel.create({...payload, academicSemester})
     return result
 }
 
