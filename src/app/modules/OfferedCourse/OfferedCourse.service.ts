ecd9706f541581d4ba817180240bea3447e4b581
Bulbul Ahammed Riad <bulbulprograming@gmail.com>
2024-06-17 16:45:52 +0600

    faculty time and date vailidatiin

--- src/app/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -8,36 +8,36 @@ import { TOfferedCourse } from "./OfferedCourse.interface";
 import { OfferedCourseModel } from "./OfferedCourse.model";
 
 
-const createOfferedCourseIntoDB = async (payload : TOfferedCourse) => {
-    const {semesterRegistration, academicFaculty, academicDepartment, course, faculty, section} = payload
-    
+const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+    const { semesterRegistration, academicFaculty, academicDepartment, course, faculty, section, days, startTime, endTime } = payload
+
     // checking semester registration is exist
     const isSemesterRegistrationExist = await SemesterRegistrationModel.findById(semesterRegistration)
-    if(!isSemesterRegistrationExist){
+    if (!isSemesterRegistrationExist) {
         throw new AppError(404, 'Semester Registration not Found!')
     }
-    
+
     // checking academic Faculty is exist
     const isAcademicFacultyExist = await academicFacultyModel.findById(academicFaculty)
-    if(!isAcademicFacultyExist){
+    if (!isAcademicFacultyExist) {
         throw new AppError(404, 'academic Faculty not Found!')
     }
-    
+
     // checking academic department is exist
     const isAcademicDepartmentExist = await AcademicDepartmentModel.findById(academicDepartment)
-    if(!isAcademicDepartmentExist){
+    if (!isAcademicDepartmentExist) {
         throw new AppError(404, 'academic department not Found!')
     }
-    
+
     // checking academic course is exist
     const isCourseExist = await courseModel.findById(course)
-    if(!isCourseExist){
+    if (!isCourseExist) {
         throw new AppError(404, 'course not Found!')
     }
-    
+
     // checking academic faculty is exist
     const isFacultyExist = await FacultyModel.findById(faculty)
-    if(!isFacultyExist){
+    if (!isFacultyExist) {
         throw new AppError(404, 'faculty not Found!')
     }
 
@@ -47,28 +47,48 @@ const createOfferedCourseIntoDB = async (payload : TOfferedCourse) => {
     const isDepartmentBelongToFaculty = await AcademicDepartmentModel.findOne({
         _id: academicDepartment,
         academicFaculty,
-      });
-    
-      if (!isDepartmentBelongToFaculty) {
+    });
+
+    if (!isDepartmentBelongToFaculty) {
         throw new AppError(
-          500,
-          `This ${isAcademicDepartmentExist.name} is not  belong to this ${isAcademicFacultyExist.name}`,
+            500,
+            `This ${isAcademicDepartmentExist.name} is not  belong to this ${isAcademicFacultyExist.name}`,
         );
-      }
+    }
 
-      // checking same semester registration same course and same section is already exist.
+    // checking same semester registration same course and same section is already exist.
 
-      const isSameOfferedCourseExistWithSameRegisteredWithSameSection = await OfferedCourseModel.findOne({
+    const isSameOfferedCourseExistWithSameRegisteredWithSameSection = await OfferedCourseModel.findOne({
         semesterRegistration,
         course,
         section
-      })
+    })
 
-      if(isSameOfferedCourseExistWithSameRegisteredWithSameSection){
+    if (isSameOfferedCourseExistWithSameRegisteredWithSameSection) {
         throw new AppError(500, 'offered course with same section is already exist!')
-      }
+    }
 
-    const result = await OfferedCourseModel.create({...payload, academicSemester})
+    const newSchedule = {
+        startTime,
+        endTime,
+        days
+    }
+
+    const existSchedule = await OfferedCourseModel.find({semesterRegistration, faculty, days : {$in : days}}).select('days startTime endTime')
+    
+    existSchedule.forEach((schedule)=>{
+        const existingStartTime =new Date(`1970-01-01T${schedule.startTime}:00`)
+        const existingEndTime =new Date(`1970-01-01T${schedule.endTime}:00`)
+        const newStartTime =new Date(`1970-01-01T${newSchedule.startTime}:00`)
+        const newEndTime =new Date(`1970-01-01T${newSchedule.startTime}:00`)
+
+        if(newStartTime <= existingEndTime && newEndTime >= existingStartTime){
+           
+            throw new AppError(500, 'This faculty is not available at that time ! chose another time or day.')
+        }
+    })
+
+    const result = await OfferedCourseModel.create({ ...payload, academicSemester })
     return result
 }
 
